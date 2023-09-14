03231c5939fd6f33a44e139e79cbab2c0b444f08
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-15 00:00:00 +0330

    FitnessFunction.java modified

---
+++ src/memetic/FitnessFunction.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memetic;
+
+import java.util.List;
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.Vm;
+
+public class FitnessFunction {
+
+    public int dimension = MA.nCloudlets;
+    private int minVal = 0;
+    private int maxVal = MA.nTotalVms;
+    
+    public int[] minBounds;	// The minimum bounds for each dimension
+    public int[] maxBounds;	// The maximum bounds for each dimension
+    public int nbEvals = 0;
+
+    // Constructor
+    public FitnessFunction() {
+        // Initialize boundaries
+        minBounds = new int[dimension];
+        maxBounds = new int[dimension];
+        for (int i = 0; i < dimension; i++) {
+            minBounds[i] = minVal;
+            maxBounds[i] = maxVal;
+        }
+    }
+
+    public double getFitnessValue(int[] results, List<Vm> vmList, List<Cloudlet> cloudletList) {
+        
+        int nTotalVms = vmList.size();
+        int nCloudlets = cloudletList.size();
+
+        int suminstructions[] = new int[nTotalVms];
+        double relativeRatio[] = new double[nTotalVms];
+
+        for (int i = 0; i < nCloudlets; i++) {
+            suminstructions[results[i]] += cloudletList.get(i).getCloudletLength();
+        }
+
+        for (int i = 0; i < nTotalVms; i++) {
+            relativeRatio[i] = suminstructions[i] / vmList.get(i).getMips();
+        }
+
+        double totalLength = 0;
+
+        for (int i = 0; i < nTotalVms; i++) {
+            totalLength += relativeRatio[i];
+        }
+
+        double mean = totalLength / MA.nTotalVms;
+
+        double sum = 0;
+
+        for (int i = 0; i < MA.nTotalVms; i++) {
+
+            sum += Math.pow((relativeRatio[i] - mean), 2);
+
+        }
+        double std = sum / MA.nTotalVms;
+
+        return std;
+    }
+
+}
